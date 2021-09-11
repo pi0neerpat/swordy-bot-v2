761b7d6e0da85b4c467ad16ba9119d322150c61e
patrick <blockchainbuddha@gmail.com>
2021-09-11 09:41:31 -0400

    dotenv

--- bot/src/bot.js
+++ bot/src/bot.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
+// require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
 const Discord = require('discord.js')
 
 const ApiMgr = require('./apiMgr')
